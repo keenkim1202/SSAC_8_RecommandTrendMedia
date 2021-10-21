445a1f399d239cd18f360b80c9b14bcfe6de1e3b
KEEN <homurahomu@naver.com>
2021-10-21 16:15:27 +0900

    detailVCd의 줄거리보기 섹션 추가

--- RecommendTrendMedia/Sources/ViewControllers/DetailViewController.swift
+++ RecommendTrendMedia/Sources/ViewControllers/DetailViewController.swift
@@ -12,11 +12,13 @@ class DetailViewController: UIViewController {
   // MARK: Properties
   var media: TvShow?
   var starring: [String] = []
+  var isOpen: Bool = false
   
   // MARK: UI
   @IBOutlet weak var headerImageView: UIImageView!
   @IBOutlet weak var posterImageView: UIImageView!
   @IBOutlet weak var detailTableView: UITableView!
+  
   
   // MARK: View Life-Cycle
   override func viewDidLoad() {
@@ -42,21 +44,43 @@ class DetailViewController: UIViewController {
 // MARK: Extension - TableViewDelegate
 extension DetailViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 80
+    if indexPath.section == 0 {
+      return tableView.estimatedRowHeight
+    } else {
+      return 80
+    }
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
   }
 }
 
 // MARK: Extension - TableViewDataSource
 extension DetailViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return  starring.count
+    if section == 0 {
+      return 1
+    } else {
+      return starring.count
+    }
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailTableViewCell.identifier) as? DetailTableViewCell else { return UITableViewCell() }
-    // 출연진 목록
-    cell.detailTitleLabel.text = starring[indexPath.row]
-    cell.cellConfigure()
-    return cell
+    if indexPath.section == 0 {
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: OverviewTableViewCell.identifier) as? OverviewTableViewCell else { return UITableViewCell() }
+      if let media = media {
+        cell.overviewLabel.text = media.overview
+      } else {
+        cell.overviewLabel.text = "줄거리 정보를 가져올 수 없습니다."
+      }
+      return cell
+    } else {
+      guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailTableViewCell.identifier) as? DetailTableViewCell else { return UITableViewCell() }
+      // 출연진 목록
+      cell.detailTitleLabel.text = starring[indexPath.row]
+      cell.cellConfigure()
+      return cell
+    }
   }
 }
---
+++ RecommendTrendMedia/Sources/Views/OverviewTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  OverviewTableViewCell.swift
+//  RecommendTrendMedia
+//
+//  Created by KEEN on 2021/10/21.
+//
+
+import UIKit
+
+class OverviewTableViewCell: UITableViewCell {
+  static let identifier: String = "overviewCell"
+
+  @IBOutlet weak var overviewLabel: UILabel!
+}
